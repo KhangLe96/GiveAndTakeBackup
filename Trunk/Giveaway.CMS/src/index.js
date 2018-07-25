0725e1ce71d725a9bf2de26573cd4c081d0f21d5
Quoc Tran <stevelee2497@gmail.com>
2018-07-25 16:32:16 +0700

    Login screen

--- Trunk/Giveaway.CMS/src/index.js
+++ Trunk/Giveaway.CMS/src/index.js
@@ -23,4 +23,42 @@ app.router(require('./router'));
 // 5. Start
 app.start('#root');
 
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '{199092104265521}',
+    cookie     : true,
+    xfbml      : true,
+    version    : '{v3.0}'
+  });
+  FB.AppEvents.logPageView();
+};
+
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0&appId=262228624561034&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+function onSuccess(googleUser) {
+  console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+function onFailure(error) {
+  console.log(error);
+}
+
 export default app._store; // eslint-disable-line
--- Trunk/Giveaway.CMS/src/routes/Auth/Login.js
+++ Trunk/Giveaway.CMS/src/routes/Auth/Login.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { Alert, Button, Form, Icon, Input, Spin } from 'antd';
 import { connect } from 'dva';
+import { GoogleLogin } from 'react-google-login-component';
 import { Link } from 'dva/router';
 
 
@@ -110,6 +111,15 @@ export default class Login extends PureComponent {
               <Button type="primary" size="large" htmlType="submit" className="login-form-button">
                 Đăng nhập
               </Button>
+            </div>
+            <div className="login_facebook_row">
+              <div className="fb-login-button" data-width="398" data-max-rows="1" data-size="large"
+                   data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false"
+                   data-use-continue-as="false"></div>
+
+            </div>
+            <div className="login_facebook_row">
+              <div className="g-signin2" data-onsuccess="onSignIn" data-width="398" data-height="200" data-max-rows="1" data-longtitle="true"></div>
             </div>
           </Form>
         </div>
