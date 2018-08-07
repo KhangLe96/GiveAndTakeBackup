495f0fe624cbba1e36c08e69c7a1a15eae67bf90
Quoc Tran <stevelee2497@gmail.com>
2018-08-07 13:48:38 +0700

    gitflow-feature-stash: Sdk

--- Software/GiveAndTake.Core/ViewModels/LoginViewModel.cs
+++ Software/GiveAndTake.Core/ViewModels/LoginViewModel.cs
@@ -1,12 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using MvvmCross.Commands;
 using MvvmCross.ViewModels;
 
 namespace GiveAndTake.Core.ViewModels
 {
     public class LoginViewModel : MvxViewModel
     {
+        public IMvxAsyncCommand FacebookLoginCommand { get; set; }
         public LoginViewModel()
         {
+
         }
     }
 }
--- Software/GiveAndTake.Droid/Views/LoginView.cs
+++ Software/GiveAndTake.Droid/Views/LoginView.cs
@@ -1,12 +1,18 @@
-﻿using Android.App;
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
-using Android.Support.V7.App;
-using Android.Views;
+using Android.Runtime;
 using Android.Widget;
 using GiveAndTake.Core.ViewModels;
 using MvvmCross.Droid.Support.V7.AppCompat;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
+using Xamarin.Facebook;
+using Xamarin.Facebook.Login;
+using Xamarin.Facebook.Login.Widget;
+using Object = Java.Lang.Object;
 
 namespace GiveAndTake.Droid.Views
 {
@@ -17,11 +23,77 @@ namespace GiveAndTake.Droid.Views
     )]
     public class LoginView : MvxAppCompatActivity<LoginViewModel>
     {
+        private ICallbackManager _callbackManager;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.LoginView);
+
+            _callbackManager = CallbackManagerFactory.Create();
+            var loginButton = FindViewById<LoginButton>(Resource.Id.login_button);
+
+            var loginCallback = new FacebookCallback<LoginResult>()
+            {
+                HandleSuccess = OnLoginSuccess,
+                HandleCancel = OnCancelLogin,
+                HandleError = OnLoginError
+            };
+
+            loginButton.RegisterCallback(_callbackManager, loginCallback);
+            LoginManager.Instance.LogInWithReadPermissions(this , new List<string>
+            {
+                "public_profile"
+            });
+            LoginManager.Instance.RegisterCallback(_callbackManager, loginCallback);
+        }
+
+        private void OnLoginSuccess(LoginResult loginResult)
+        {
+            var profile = Profile.CurrentProfile;
+            var tvName = FindViewById<TextView>(Resource.Id.tvName);
+            tvName.Text = profile.Name;
+        }
+
+        private void OnCancelLogin()
+        {
+        }
+
+        private void OnLoginError(FacebookException loginException)
+        {
+        }
+
+       protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            _callbackManager.OnActivityResult(requestCode, (int) resultCode, data);
+        }
+    }
+
+
+   public class FacebookCallback<TResult> : Object, IFacebookCallback where TResult : Object
+    {
+        public Action HandleCancel { get; set; }
+        public Action<FacebookException> HandleError { get; set; }
+        public Action<TResult> HandleSuccess { get; set; }
+
+        public void OnCancel()
+        {
+            var c = HandleCancel;
+            c?.Invoke();
+        }
+
+        public void OnError(FacebookException error)
+        {
+            var c = HandleError;
+            c?.Invoke(error);
+        }
+
+        public void OnSuccess(Object result)
+        {
+            var c = HandleSuccess;
+            c?.Invoke(result.JavaCast<TResult>());
         }
     }
 }
