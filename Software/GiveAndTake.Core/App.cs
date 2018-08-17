f6a08133f5331babe7395aad39382357c128af6c
Quoc Tran <stevelee2497@gmail.com>
2018-08-17 10:46:46 +0700

    disable facebook ios sdk

--- Software/GiveAndTake.Core/App.cs
+++ Software/GiveAndTake.Core/App.cs
@@ -1,4 +1,5 @@
 ﻿using GiveAndTake.Core.ViewModels;
+using GiveAndTake.Core.ViewModels.Base;
 using MvvmCross.IoC;
 using MvvmCross.ViewModels;
 
@@ -13,7 +14,7 @@ namespace GiveAndTake.Core
 				.AsInterfaces()
 				.RegisterAsLazySingleton();
 
-			RegisterAppStart<HomeViewModel>();
+			RegisterAppStart<MasterViewModel>();
 		}
 	}
 }
