1b475bc4a767f78a5775ccba09f7a38dbfbf8577
rocklaam <Lam.Nguyen@sioux.asia>
2018-08-27 10:53:08 +0700

    140_ Create tab navigation IOS + Create TabNavigationView.cs to hold tabs + Add image and image@2x to Resource to set icon for tabs + Edit mvx attribute of homeview.cs--> become a tab layout + Add code line: NavigationController?.SetNavigationBarHidden(true, animated); to control the show of NavigationBar in child screen and tab navigation screen. + Add 4 tabs and files view and viewModel for this. + Create TabNavigation folder for View iOS and ViewModel and migrate tabs files into it

---
+++ Software/GiveAndTake.iOS/Views/TabNavigation/NotificationView.cs
@@ -0,0 +1,43 @@
+﻿using GiveAndTake.iOS.Helpers;
+using GiveAndTake.iOS.Views.Base;
+using MvvmCross.Platforms.Ios.Presenters.Attributes;
+using UIKit;
+
+namespace GiveAndTake.iOS.Views.TabNavigation
+{
+	[MvxTabPresentation(TabIconName = "Images/notification_off",
+		TabSelectedIconName = "Images/notification_on",
+		WrapInNavigationController = true)]
+	public class NotificationView : BaseView
+	{
+		protected override void InitView()
+		{
+			UILabel testLabel = UIHelper.CreateLabel(UIColor.Black, DimensionHelper.MediumTextSize);
+			testLabel.Text = "Notification View";
+			View.Add(testLabel);
+			View.AddConstraints(new[]
+			{
+				NSLayoutConstraint.Create(testLabel,
+					NSLayoutAttribute.CenterX,
+					NSLayoutRelation.Equal,
+					View,
+					NSLayoutAttribute.CenterX,
+					1,
+					0),
+				NSLayoutConstraint.Create(testLabel,
+					NSLayoutAttribute.CenterY,
+					NSLayoutRelation.Equal,
+					View,
+					NSLayoutAttribute.CenterY,
+					1,
+					0)
+			});
+		}
+
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(animated);
+			NavigationController?.SetNavigationBarHidden(true, animated);
+		}
+	}
+}
