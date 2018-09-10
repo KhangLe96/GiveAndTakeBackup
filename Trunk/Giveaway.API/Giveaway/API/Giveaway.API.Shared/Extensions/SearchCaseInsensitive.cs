eb3030d1bb756fe5f072816f47038353dcee4014
Khang Le <khang.le@sioux.asia>
2018-09-10 18:28:27 +0700

    [API] Search post by keyword

---
+++ Trunk/Giveaway.API/Giveaway/API/Giveaway.API.Shared/Extensions/SearchCaseInsensitive.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Giveaway.API.Shared.Extensions
+{
+    public static class SearchCaseInsensitive
+    {
+        public static bool Contains(this string target, string value, StringComparison comparison)
+        {
+            return target.IndexOf(value, comparison) >= 0;
+        }
+    }
+}
