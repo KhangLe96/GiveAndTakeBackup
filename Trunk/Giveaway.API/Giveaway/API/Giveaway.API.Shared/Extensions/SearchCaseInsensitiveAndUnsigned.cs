3ff8c278f2ad66c9813a0f3180c8cca98c948268
Khang Le <khang.le@sioux.asia>
2018-10-03 10:32:22 +0700

    [API] 226: Search post by unsigned character

---
+++ Trunk/Giveaway.API/Giveaway/API/Giveaway.API.Shared/Extensions/SearchCaseInsensitiveAndUnsigned.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Giveaway.API.Shared.Extensions
+{
+    public static class SearchCaseInsensitiveAndUnsigned
+    {
+        public static bool Contains(this string target, string value, StringComparison comparison)
+        {
+            return ConvertToUnSign(target).IndexOf(ConvertToUnSign(value), comparison) >= 0;
+        }
+
+        public static string ConvertToUnSign(string input)
+        {
+            input = input.Trim();
+            for (int i = 0x20; i < 0x30; i++)
+            {
+                input = input.Replace(((char)i).ToString(), " ");
+            }
+            Regex regex = new Regex(@"\p{IsCombiningDiacriticalMarks}+");
+            string str = input.Normalize(NormalizationForm.FormD);
+            string str2 = regex.Replace(str, string.Empty).Replace('đ', 'd').Replace('Đ', 'D');
+            while (str2.IndexOf("?") >= 0)
+            {
+                str2 = str2.Remove(str2.IndexOf("?"), 1);
+            }
+            return str2;
+        }
+    }
+}
