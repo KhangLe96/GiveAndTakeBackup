130e0d0a61827fa5bad132f552bc2ed91c084d83
Thanh Vo <thanh.vo@sioux.asia>
2018-10-29 09:23:17 +0700

    245 Review Change to "just now /minutes ago" / "title" in listview.

--- Software/GiveAndTake.Core/Helpers/TimeHelper.cs
+++ Software/GiveAndTake.Core/Helpers/TimeHelper.cs
@@ -11,14 +11,17 @@ namespace GiveAndTake.Core.Helpers
 			var timeSpan = DateTime.Now.Subtract(dateTime);
 			if (timeSpan <= TimeSpan.FromSeconds(60))
 			{
+				//Review ThanhVo In the case  = 60s, should use minute
 				return "vài giây trước";
 			}
 			if (timeSpan <= TimeSpan.FromMinutes(60))
 			{
+				//Review ThanhVo In the case  = 60mins, should use hour
 				return $"{timeSpan.Minutes} phút trước"; 				
 			}
 			if (timeSpan <= TimeSpan.FromHours(24))
 			{
+				//Review ThanhVo In the case  = 24hours, should use day (yesterday)
 				return $"{timeSpan.Hours} giờ trước";
 			}			
 			return dateTime.ToString("dd.MM.yyyy");
