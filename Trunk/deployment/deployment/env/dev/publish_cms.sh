262d3639a7662640bceea0217395e23b34aaad4b
root <root@GiveAndTake.hb2r25ky331e1m3z0mc4qnnmha.ix.internal.cloudapp.net>
2019-03-14 20:00:16 +0000

    dfd

---
+++ Trunk/deployment/deployment/env/dev/publish_cms.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+DEPLOY_ENVIRONMENT=dev
+CURRENT_PATH=$(pwd)
+OUTPUT_PATH="${CURRENT_PATH}/build_output/cms/"
+REMOTE_PATH=/home/chovanhan/projects/giveandtake/Trunk/deployment/deployment/env/${DEPLOY_ENVIRONMENT}
+REMOTE_PROJECT_PATH=/home/chovanhan/projects/giveandtake/Trunk/deployment/deployment/env/${DEPLOY_ENVIRONMENT}/output/cms
+REMOTE_ID=chovanhan@13.76.45.56
+REMOTE_PORT=22
+PROJECT_PATH="../../../../Giveaway.CMS"
+DEPLOY_KEY=./key/id_rsa
+
+echo "THIS WILL DEPLOY ON: ${DEPLOY_ENVIRONMENT}"
+
+echo "MOVING to project path"
+cd ${PROJECT_PATH}
+
+echo "CHECKOUT master"
+git checkout develop
+git pull origin develop
+
+echo "BUILDING"
+npm install && npm run build
+
+mkdir ${OUTPUT_PATH}
+echo "COPY TO OUTPUT $(pwd) to ${OUTPUT_PATH}"
+cp -r dist/* ${OUTPUT_PATH}
+
+cd "${CURRENT_PATH}"
+
+read -r -p "Sending build to server and restart service now? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY])
+		scp -C -i ${DEPLOY_KEY} -P ${REMOTE_PORT} -r build_output/cms/* ${REMOTE_ID}:${REMOTE_PROJECT_PATH}
+		ssh -i ${DEPLOY_KEY} -p ${REMOTE_PORT} ${REMOTE_ID} "cd ${REMOTE_PATH};docker-compose up -d --build giveandtake-cms"
+		;;
+    *)
+        echo "not sending data, service not restarted. Exiting ..."
+    ;;
+esac
+
+echo "========================== Deploy successfully ========================="
---
+++ Trunk/deployment/deployment/env/dev/volumes/config/cms/config.js
@@ -0,0 +1,3 @@
+window.config = {
+    apiUrl: 'https://cms.giveandtake.tk'
+};
